829496106b5b98270e12d15d4dc10a9bc5ff0a70
pxly <pxly@uiowa.edu>
2025-09-12 15:56:46 -0500

    From docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,17 @@
-npm install amplify-adapter
+import adapter from 'amplify-adapter';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			out: 'build'
-		})
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
