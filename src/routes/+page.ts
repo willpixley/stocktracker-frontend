f97e4e1a93906450b8a2f47bc680fb1bc1d54410
pxly <pxly@uiowa.edu>
2025-09-12 15:10:07 -0500

    Base url

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export async function load({ params }: { params: any }) {
 	const BASE_URL = import.meta.env.VITE_BACKEND_API_URL;
+	console.log('Base url', BASE_URL);
 
 	try {
 		const trades = await axios.get(`${BASE_URL}/trades`);
